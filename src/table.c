0a69545dd8a089d15396e51f89d5cce3b15ecb60
Aiden <ayeats097@gmail.com>
2025-07-14 19:02:35 +0300

    added some minor changes to the hashtable

--- src/table.c
+++ src/table.c
@@ -1,19 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
+#include <SDL2/SDL.h>
 #include "table.h"
-
-typedef struct entry{
-    char *key;
-    void *value;
-    struct entry *next;
-} entry_t;
-
-typedef struct {
-    entry_t **entries;
-    unsigned int count;
-    unsigned int length;
-    unsigned int (*hash_function)(char *key);
-} hash_table_t;
 
 unsigned int jenkins_one_at_a_time_hash(char *key){
     unsigned int hash = 0;
@@ -28,68 +16,106 @@ unsigned int jenkins_one_at_a_time_hash(char *key){
     return hash;
 }
 
-hash_table_t *init_hashtable(unsigned int (*hash_function)(char *key), unsigned int length){
-    hash_table_t *ht = malloc(sizeof(hash_table_t));
-
-    ht->entries = calloc(length, sizeof(entry_t*));
-    ht->count = 0;
-    ht->length = length;
-    ht->hash_function = hash_function;
-
-    return ht;
-}
-
 unsigned int hash(hash_table_t *ht, char *key){
     return ht->hash_function(key) % ht->length;
 }
 
-entry_t *pair_value(char *key, void *value, unsigned int value_size){
+void free_value(entry_t *entry){
+    switch (entry->type) { 
+        case SURFACE:
+            SDL_FreeSurface(entry->value);
+        break;
+        case TEXTURE:
+            SDL_DestroyTexture(entry->value);
+        break;
+        case OTHER:
+            free(entry->value);
+        break;
+    }
+}
+
+void free_entry(entry_t *entry){
+    free(entry->key);
+    free_value(entry);
+    
+    while (entry)
+}
+
+void set_value(entry_t *entry, void *value, unsigned int value_size){
+    entry->value = malloc(value_size);
+    memcpy(entry->value, value, value_size);
+}
+
+void set_key(entry_t *entry, char *key){
+    entry->key = malloc(strlen(key) + 1); /* +1 because of the null terminator */
+    strcpy(entry->key, key);
+}
+
+entry_t *pair_value(char *key, void *value, unsigned int value_size, Value_type type){
     entry_t *entry = malloc(sizeof(entry_t));
 
-    entry->key = malloc(strlen(key) + 1); /* +1 because of the null terminator */
-    entry->value = malloc(value_size);
-
-    strcpy(entry->key, key);
-    memcpy(entry->value, value, value_size);
-
+    set_key(entry, key);
+    set_value(entry, value, value_size);
     entry->next = NULL;
+    entry->type = type;
 
     return entry;
 }
 
-void set_value(hash_table_t *ht, char *key, void *value, unsigned int value_size){
+void insert(hash_table_t *ht, char *key, void *value, unsigned int value_size, Value_type type){
     unsigned int index = hash(ht, key);
 
     entry_t *entry = ht->entries[index];
 
     if (entry == NULL){
-        ht->entries[index] = pair_value(key, value, value_size);
-        return;
-    }
-
-    if (strcmp(key, entry->key) == 0){
-        free(entry->value);
-        entry->value = malloc(value_size);
-        memcpy(entry->value, value, value_size);
+        ht->entries[index] = pair_value(key, value, value_size, type);
         return;
     }
 
     entry_t *prev_entry;
 
     while (entry != NULL) {
+        if (strcmp(key, entry->key) == 0){
+            free_value(entry);
+            set_value(entry, value, value_size);
+            return;
+        }
         prev_entry = entry;
         entry = entry->next;
     }
 
-    prev_entry->next = pair_value(key, value, value_size);
+    prev_entry->next = pair_value(key, value, value_size, type);
 }
 
-void *get_value(hash_table_t *ht, char *key){
+void *get(hash_table_t *ht, char *key){
     unsigned int index = hash(ht, key);
 
     entry_t *entry = ht->entries[index];
 
-    if (entry != NULL ){
+    if (entry == NULL){
+        return NULL;
+    }
+
+    entry_t *prev_entry;
+
+    while (entry != NULL){
+        if (strcmp(key, entry->key) == 0){
+            return entry->value;
+        }
+
+        prev_entry = entry;
+        entry = entry->next;
+    }
+
+    return NULL;
+}
+
+void *delete(hash_table_t *ht, char *key){
+    unsigned int index = hash(ht, key);
+
+    entry_t *entry = ht->entries[index];
+
+    if (entry == NULL){
         return NULL;
     }
 
@@ -105,4 +131,24 @@ void *get_value(hash_table_t *ht, char *key){
     }
 
     return NULL;
+}
+
+void resize(hash_table_t ht, unsigned int length){
+
+}
+
+hash_table_t *init_hashtable(unsigned int (*hash_function)(char *key), unsigned int length){
+    hash_table_t *ht = malloc(sizeof(hash_table_t));
+
+    ht->entries = malloc(length * sizeof(entry_t*));
+    for (int i=0;i<length * sizeof(entry_t*);i+=sizeof(entry_t*)){
+        ht->entries[i] = NULL;
+    }
+    ht->count = 0;
+    ht->length = length;
+    ht->hash_function = hash_function;
+    ht->insert = insert;
+    ht->get = get;
+
+    return ht;
 }
--- src/table.h
+++ src/table.h
@@ -1,6 +1,33 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
+typedef enum{
+    SURFACE,
+    TEXTURE,
+    OTHER /* Literally any standard variable type that C has */
+} Value_type;
+
+typedef struct entry{
+    char *key;
+    void *value;
+
+    struct entry *next;
+
+    Value_type type;
+} entry_t;
+
+typedef struct hash_table{
+    unsigned int count;
+    unsigned int length;
+
+    entry_t **entries;
+
+    unsigned int (*hash_function)(char *key);
+    void (*insert)(struct hash_table *ht, char *key, void *value, unsigned int value_size, Value_type type);
+    void* (*get)(struct hash_table *ht, char *key);
+} hash_table_t;
+
 unsigned int jenkins_one_at_a_time_hash(char *key);
+hash_table_t *init_hashtable(unsigned int (*hash_function)(char *key), unsigned int length);
 
 #endif
